45ad875843837d2d7a16ac47409e08a9c324c912
amirz <amirzahavi90@gmail.com>
2020-03-19 02:49:40 +0200

    fix tests

--- packages/server/__tests__/server.test.js
+++ packages/server/__tests__/server.test.js
@@ -10,63 +10,73 @@ jest.mock('express', () => {
     use: jest.fn()
   });
 });
+const routeHandlerSpy = jest.fn();
+process.env.GUARD_JET_TOKEN = 'empty';
 
-
-const {Server} = require('..');
+const {createServer} = require('..');
 
 describe('server', () => {
-  it('should throw error when service name is not defined', () => {
-    expect(() => new Server()).toThrowError('service name must be provided');
+  it('should throw error when service name is not defined', async () => {
+    expect.assertions(1);
+    try {
+      await createServer(null);
+    } catch (e) {
+      expect(e.message).toEqual('service name must be provided');
+    }
   });
 
-  it('should exists', () => {
-    const s = new Server('test');
+  it('should exists', async () => {
+    const s = await createServer('test');
     expect(s).toBeDefined();
   });
 
-  it('should call listen on express with port', () => {
-    const s = new Server('test');
+  it('should call listen on express with port', async () => {
+    const s = await createServer('test');
     s.listen(1234);
     expect(s.app.listen.mock.calls.length).toEqual(1);
     expect(s.app.listen.mock.calls[0][0]).toEqual(1234);
   });
 
-  it('should have default route GET /healthcheck', () => {
-    const s = new Server('test');
+  it('should have default route GET /healthcheck', async () => {
+    const s = await createServer('test');
     expect(s.app.get.mock.calls.length).toBeGreaterThanOrEqual(1);
     expect(s.app.get.mock.calls[0][0]).toEqual('/helthcheck');
   });
 
-  it('should have a GET route', () => {
-    const s = new Server('test');
-    s.get('/get-route', null);
+  it('should have a GET route', async () => {
+    const s = await createServer('test');
+    s.get('/get-route', routeHandlerSpy);
     expect(s.app.get.mock.calls.length).toBeGreaterThanOrEqual(1);
     expect(s.app.get.mock.calls[1][0]).toEqual('/get-route');
+    expect(s.app.get.mock.calls[1][2]).toStrictEqual([routeHandlerSpy]);
   });
 
-  it('should have a POST route', () => {
-    const s = new Server('test');
-    s.post('/post-route', null);
+  it('should have a POST route', async () => {
+    const s = await createServer('test');
+    s.post('/post-route', routeHandlerSpy);
     expect(s.app.post.mock.calls.length).toEqual(1);
     expect(s.app.post.mock.calls[0][0]).toEqual('/post-route');
+    expect(s.app.post.mock.calls[0][2]).toStrictEqual([routeHandlerSpy]);
   });
 
-  it('should have a PUT route', () => {
-    const s = new Server('test');
-    s.put('/put-route', null);
+  it('should have a PUT route', async () => {
+    const s = await createServer('test');
+    s.put('/put-route', routeHandlerSpy);
     expect(s.app.put.mock.calls.length).toEqual(1);
     expect(s.app.put.mock.calls[0][0]).toEqual('/put-route');
+    expect(s.app.put.mock.calls[0][2]).toStrictEqual([routeHandlerSpy]);
   });
 
-  it('should have a DELETE route', () => {
-    const s = new Server('test');
-    s.delete('/delete-route', null);
+  it('should have a DELETE route', async () => {
+    const s = await createServer('test');
+    s.delete('/delete-route', routeHandlerSpy);
     expect(s.app.delete.mock.calls.length).toEqual(1);
     expect(s.app.delete.mock.calls[0][0]).toEqual('/delete-route');
+    expect(s.app.delete.mock.calls[0][2]).toStrictEqual([routeHandlerSpy]);
   });
 
-  it('should have a use route', () => {
-    const s = new Server('test');
+  it('should have a use route', async () => {
+    const s = await createServer('test');
     const routeHandlerSpy = jest.fn();
     s.use(routeHandlerSpy);
     expect(s.app.use.mock.calls.length).toBeGreaterThanOrEqual(1);
