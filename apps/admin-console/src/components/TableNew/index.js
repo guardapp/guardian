25129e45360a0fbc71af4fd3b9f017a3d86d25e3
amirz <amirzahavi90@gmail>
2020-04-11 03:41:00 +0300

    use new table

--- apps/admin-console/src/components/TableNew/index.js
+++ apps/admin-console/src/components/TableNew/index.js
@@ -1,17 +1,26 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useLayoutEffect } from 'react';
 import './style.css';
 
 import { useTable, useSortBy, useFilters } from 'react-table';
+import { useInView } from 'react-intersection-observer';
+
 import SortIcon from './SortIcon';
 import SearchIcon from './SearchIcon';
 
-export default function Table({ columns, data }) {
+export default function Table({ columns, data, hasMore, fetchMore }) {
 	columns.forEach((col) => {
 		col.Filter = SearchIcon;
 		col.filter = 'includes';
 	});
 	const cols = useMemo(() => columns, [columns]);
 	const _data = useMemo(() => data, [data]);
+	const [ref, inView] = useInView({ triggerOnce: true });
+
+	useLayoutEffect(() => {
+		if (inView && hasMore) {
+			fetchMore();
+		}
+	}, [inView, hasMore, fetchMore]);
 
 	const { getTableProps, rows, prepareRow, headers } = useTable(
 		{
@@ -29,7 +38,7 @@ export default function Table({ columns, data }) {
 			{...getTableProps()}
 		>
 			{headers.map((column) => (
-				<div className="table__cell table__cell--header">
+				<div key={column.id} className="table__cell table__cell--header">
 					<div>{column.render('Header')}</div>
 					<div className="table__actions">
 						{column.render('Filter')}
@@ -44,7 +53,11 @@ export default function Table({ columns, data }) {
 			{rows.map((row) => {
 				prepareRow(row);
 				return (
-					<div className="table__row" {...row.getRowProps()}>
+					<div
+						className="table__row"
+						{...row.getRowProps()}
+						style={{ gridTemplateColumns: `repeat(${cols.length}, 1fr)` }}
+					>
 						{row.cells.map((cell) => {
 							return (
 								<div className="table__cell" {...cell.getCellProps()}>
@@ -55,6 +68,7 @@ export default function Table({ columns, data }) {
 					</div>
 				);
 			})}
+			<div className="table__end" ref={ref}></div>
 		</div>
 	);
 }
--- apps/admin-console/src/pages/Users/UserTable.js
+++ apps/admin-console/src/pages/Users/UserTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 
@@ -8,7 +8,7 @@ import Notification from '../../components/Notification';
 
 const ALL_USERS_IN_ROLE = gql`
 	query GetAllAdmins($role: Role!, $offset: Int!) {
-		users(role: $role, paginate: { limit: 10, offset: $offset }) {
+		users(role: $role, paginate: { limit: 100, offset: $offset }) {
 			id
 			email
 		}
@@ -19,30 +19,35 @@ const columns = [
 	{ Header: 'Id', accessor: 'id' },
 	{ Header: 'Email', accessor: 'email' },
 ];
-// function useScrollPosition() {
-// 	const [pos, setPos] = useState(window.scrollY);
-
-// 	useLayoutEffect(() => {
-// 		function setPosition() {
-// 			setPos(window.scrollY);
-// 		}
-
-// 		window.addEventListener('scroll', setPosition);
-// 		return () => {
-// 			window.removeEventListener('scroll', setPosition);
-// 		};
-// 	});
-
-// 	return [pos];
-// }
 
 export default function UserTable({ role }) {
 	const [hasMore, setHasMore] = useState(true);
+
+	useEffect(() => {
+		setHasMore(true);
+	}, [role]);
 
 	const { loading, error, data, fetchMore } = useQuery(ALL_USERS_IN_ROLE, {
 		variables: { offset: 0, role },
 		notifyOnNetworkStatusChange: true,
 	});
+
+	function fetchMoreUsers() {
+		fetchMore({
+			variables: { offset: data.users.length, role },
+			updateQuery: (prev, { fetchMoreResult }) => {
+				setHasMore(false);
+				if (!fetchMoreResult || !fetchMoreResult.users || fetchMoreResult.users.length === 0) {
+					return prev;
+				}
+
+				setHasMore(true);
+				return Object.assign({}, prev, {
+					users: [...prev.users, ...fetchMoreResult.users],
+				});
+			},
+		});
+	}
 
 	if (loading) {
 		return <Spinner active={loading}></Spinner>;
@@ -56,5 +61,7 @@ export default function UserTable({ role }) {
 		);
 	}
 
-	return <Table columns={columns} data={data.users}></Table>;
+	return (
+		<Table columns={columns} data={data.users} hasMore={hasMore} fetchMore={fetchMoreUsers}></Table>
+	);
 }
